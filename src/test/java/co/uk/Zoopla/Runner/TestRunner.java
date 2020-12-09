1bbbdf448300bb76f1a615152a2168ab75ddc8ad
Paul Cole <Kmintsltd@gmail.com>
2020-12-09 14:06:43 -0800

    done

--- src/test/java/co/uk/Zoopla/Runner/TestRunner.java
+++ src/test/java/co/uk/Zoopla/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
                  features={"src/test/java/co/uk/Zoopla/features"}
-                 ,plugin={"Pretty","json:target/report.json","de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber"
+                 ,plugin={"pretty","json:target/report.json","de.monochromata.cucumber.report.prettyReports:target/pretty-cucumber"
                   },glue={"co/uk/Zoopla/stepDefinitions"})
 
 public class TestRunner {
